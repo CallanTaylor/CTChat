49eef9d788ea500bce487274d1ce24b139bbc1c4
CallanTaylor <taylorcallan96@gmail.com>
2018-04-15 20:49:48 +1000

    release build

--- app/src/main/java/com/callan/taylor/ctchat/ContactsActivity.java
+++ app/src/main/java/com/callan/taylor/ctchat/ContactsActivity.java
@@ -29,6 +29,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 import java.security.Provider;
 import java.text.SimpleDateFormat;
